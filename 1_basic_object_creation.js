230fd46dd854e92da1acb36288f30a694d619fe0
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-03-19 13:24:37 +0600

    first commit

---
+++ 1_basic_object_creation.js
@@ -0,0 +1,69 @@
+//                                      :::Basic object creation:::
+
+// var obj = {};
+// var obj2 = new Object;
+// console.log(obj);
+// console.log(obj2);
+
+
+// var obj = {
+//     name: 'object',
+//     'type': 'variable',
+//     "5isTrue": true,
+//     "propertyNo.": 4,
+//     1: 5,
+//     7: 'seven'
+// };
+// console.log(obj['7']);
+// console.log(obj['propertyNo.']);
+// console.log(obj['5isTrue']);
+
+
+// const myVariable = 'color';
+// const bicycle = {
+//     color: 'blue',
+//     wheels: {
+//         diameter: 18,
+//         width: 8
+//     },
+// };
+// console.log(bicycle.color);
+// console.log(bicycle['color']);
+// console.log(bicycle.wheels);
+// console.log(bicycle['wheels']);
+// console.log(bicycle.wheels.width);
+// console.log(bicycle.wheels['diameter']);
+// console.log(bicycle['wheels']['width']);
+// console.log(bicycle[myVariable]);
+
+
+var book = {
+    name: 'Functional javaScript',
+    author: 'Michael Fogus',
+    publisher: 'O\' Reilley',
+    page: 250,
+
+    print: function() {
+        console.log(this.name +': ' + this.author);
+        
+        
+    }
+};
+
+// console.log(book);
+// console.log(book.publisher);
+// book.print();
+// console.log(book.publishedYear);
+book.publishedYear = 2010;
+// console.log(book.publishedYear);
+book['price'] = 30;
+// console.log('$' + book.price);
+// console.log(book);
+
+//object traverse:::
+for(var props in book) {
+
+    // console.log(props);
+    console.log(props + ': ' + book[props]);
+  
+}
