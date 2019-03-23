ccf1b796250d53e6230634cb5a3657e301d4715f
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-03-23 21:12:10 +0600

    Added file#5

---
+++ 5_Extracting_Properties_and_Values.js
@@ -0,0 +1,28 @@
+//:::::::::::::::::::::Extracting Properties and Values:::::::::::::::::::::::::::
+
+//:::::::::Object.keys() and Object.values()::::::::::::::
+const dictionary = {
+    car: 'automobile',
+    apple: 'healthy snack',
+    cat: 'cute furry animal',
+    dog: 'best friend',
+    pages: 1000
+};
+
+console.log(Object.keys(dictionary));
+console.log(Object.values(dictionary));
+
+// const keysArray = Object.keys(dictionary);
+// console.log(keysArray);
+
+// const valuesArray = Object.values(dictionary);
+// console.log(valuesArray);
+
+//:::::::::::extract 'keys' using 'for in' loop:::::::::::
+const result = [];
+
+for(const keys in dictionary) {
+    result.push(keys);
+}
+console.log(result);
+
