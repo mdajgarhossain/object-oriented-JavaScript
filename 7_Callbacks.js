37d1a61eebde6f8a2a59faaf77a3d800c640a1df
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-03-29 00:26:02 +0600

    Added file #7

---
+++ 7_Callbacks.js
@@ -0,0 +1,67 @@
+//:::::::::::::::::::::::Callback Functions:::::::::::::::::::::::::::::::::::::
+
+// function callAndAdd(n, callbackFunction) {
+//     return n + callbackFunction();
+// }
+
+// function returnsThree() {
+//     return 3;
+// }
+
+// console.log(callAndAdd(2, returnsThree));
+
+//:::::::::::::::::::::Array Methods:::::::::::::::::::::::::::
+
+
+//:::::::::::::::forEach() method::::::::::::::
+
+// function logIfOdd(n) {
+//     if(n % 2 !== 0) {
+//         console.log(n);
+//     }
+// }
+// logIfOdd(2);
+// logIfOdd(7);
+
+// myArray = [1, 5, 2, 4, 6, 3];
+
+// myArray.forEach(function logIfOdd(n) {
+//     if(n % 2 !== 0) {
+//         console.log(n);
+//     }
+// });
+
+
+// myArray.forEach(function (n) {
+//     if(n % 2 !== 0) {
+//         console.log(n);
+//     }
+// });
+
+
+// myArray.forEach(logIfOdd);
+
+//::::::another example::::::::
+
+// const favoriteFlavors = ['cookie dough', 'salted caramel', 'toffee'];
+
+// favoriteFlavors.forEach(function(flavor) {
+//     //debugger
+//     console.log(`I enjoy ${flavor} ice cream.`);
+// });
+
+
+//::::::::::::::map() method::::::::::::::
+
+const names = ['Jewell', 'Karim', 'Rahim'];
+
+// names.map(function(name) {
+//     console.log(`My name is ${name}.`);
+// });
+
+const nameLengths = names.map(function(name) {
+    return name.length;
+});
+console.log(nameLengths);
+
+
