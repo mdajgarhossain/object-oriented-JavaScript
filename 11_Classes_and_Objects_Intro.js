ce3c8851704e650b16b6af3581f5d7d7b7d264a1
mdajgarhossain <mdajgarhossain1@gmail.com>
2019-04-06 17:06:05 +0600

    Beginning of the object-oriented JavaScript

---
+++ 11_Classes_and_Objects_Intro.js
@@ -0,0 +1,47 @@
+//::::::::::::::::::::::::::::::::Classes and Objects:::::::::::::::::::::::::::::::::::
+
+//::::::::::::Properties and Methods:::::::::::::::::::::
+
+// const dog = {
+//     name: 'Dill Pickle',
+//     age:2,
+//     breed: 'cheagle',
+//     shake: function() {
+//         console.log(`${this.name} shakes hand!`);
+        
+//     }
+// };
+// console.log(dog);
+// dog.shake();
+
+
+// const mobile = {
+//     brand: 'Samsung',
+//     color: 'Black',
+//     ring: function() {
+//         console.log(`${mobile.brand} is ringing...`);
+        
+//     }
+// };
+// console.log(mobile);
+// mobile.ring();
+
+
+
+const flower = {
+    leafColor: 'green',
+    smell: 'fragrant',
+    grow: function() {
+        console.log('growing taller!');
+        
+    },
+    convertLight: function() {
+        console.log('converting light!');
+        
+    }
+};
+console.log(flower.smell);
+flower.convertLight();
+
+
+//In the next section, we'll take a dive into object-oriented JavaScript, beginning with constructor functions  >>>
